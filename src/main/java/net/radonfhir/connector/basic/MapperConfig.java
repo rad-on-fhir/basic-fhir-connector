4f5a1cbf5b6a9eddbda4963b374fba8329ef37d6
Alexander Drexler <adr@sourceblock.de>
2022-12-22 15:13:18 +0100

    added first HL7 Patient Mapping Test, Refactored methods to interfaces

--- src/main/java/net/radonfhir/connector/basic/MapperConfig.java
+++ src/main/java/net/radonfhir/connector/basic/MapperConfig.java
@@ -6,6 +6,7 @@ import net.radonfhir.connector.base.hl7.mapper.AppointmentSIUMapper;
 import net.radonfhir.connector.base.hl7.mapper.PatientAdt231Mapper;
 import net.radonfhir.connector.base.hl7.mapper.PatientAdt25Mapper;
 import net.radonfhir.connector.base.organization.OrganizationService;
+import net.radonfhir.connector.base.patient.loader.FhirClientLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -14,14 +15,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MapperConfig {
 
+
     @Bean
     @Autowired
     @ConditionalOnProperty(
             value = "hl7.mapper.siu",
             havingValue = "true"
     )
-    public AppointmentSIUMapper appointmentSIUMapper(OrganizationService orga, HL7Configuration config, IGenericClient client) {
-        return new AppointmentSIUMapper(orga, config, client);
+    public AppointmentSIUMapper appointmentSIUMapper(OrganizationService orga, HL7Configuration config, IGenericClient client, FhirClientLoader fhirClient) {
+        return new AppointmentSIUMapper(orga, config, client, fhirClient);
     }
 
     @Bean
@@ -30,8 +32,8 @@ public class MapperConfig {
             value ="mllp.version",
             havingValue = "2.3.1"
     )
-    public PatientAdt231Mapper patientAdt231Mapper(OrganizationService organizationService, HL7Configuration configuration, IGenericClient fhirClient) {
-        return new PatientAdt231Mapper(organizationService, configuration, fhirClient);
+    public PatientAdt231Mapper patientAdt231Mapper(OrganizationService organizationService, HL7Configuration configuration, FhirClientLoader fhirClient) {
+        return new PatientAdt231Mapper(organizationService, configuration, fhirClient, fhirClient);
     }
 
     @Bean
@@ -41,8 +43,8 @@ public class MapperConfig {
             havingValue = "2.5",
             matchIfMissing = true
     )
-    public PatientAdt25Mapper patientAdt25Mapper(OrganizationService organizationService, HL7Configuration configuration, IGenericClient fhirClient) {
-        return new PatientAdt25Mapper(organizationService, configuration, fhirClient);
+    public PatientAdt25Mapper patientAdt25Mapper(OrganizationService organizationService, HL7Configuration configuration, FhirClientLoader fhirClient) {
+        return new PatientAdt25Mapper(organizationService, configuration, fhirClient, fhirClient);
     }
 
 
